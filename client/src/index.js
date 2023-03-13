d283cda1129b4bcec78098a4c2061791db42fb40
cherry-aisha <113786104+cherry-aisha@users.noreply.github.com>
2023-03-13 20:26:06 +0000

    npm update

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "@material-tailwind/react/tailwind.css";
-import './App';
+import './App.css';
+
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
