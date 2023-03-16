debe67a3de4e1c85c779b9eec1ebc3bd2f51c5e8
elleserdonaldson <ellessedon@gmail.com>
2023-03-16 18:32:37 +0000

    here we removed signup function and the submit functionality as we will be calling it into one page from login

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -7,13 +7,6 @@ import { ADD_USER } from "../../utils/mutations";
 import Auth from "../../utils/auth";
 
 const Signup = () => {
-  const [formState, setFormState] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-  const [addUser, { error, data }] = useMutation(ADD_USER);
-
   const handleChange = (event) => {
     const { name, value } = event.target;
 
@@ -21,21 +14,6 @@ const Signup = () => {
       ...formState,
       [name]: value,
     });
-  };
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formState);
-
-    try {
-      const { data } = await addUser({
-        variables: { ...formState },
-      });
-
-      Auth.login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
-    }
   };
 
   return (
@@ -50,7 +28,7 @@ const Signup = () => {
                 <Link to="/">back to the homepage.</Link>
               </p>
             ) : (
-              <form onSubmit={handleFormSubmit}>
+              <form onSubmit={handleSignUpSubmit}>
                 <input
                   className="form-input"
                   placeholder="Your username"
