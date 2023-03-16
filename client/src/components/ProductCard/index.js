ff1e17c72157b7949532f4c40f76b18175875d9c
VascoMiguens <v_miguens@hotmail.com>
2023-03-16 20:43:44 +0000

    - adapted productcard to get the correct details

--- client/src/components/ProductCard/index.js
+++ client/src/components/ProductCard/index.js
@@ -1,28 +1,29 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const ProductCard = ( props ) => {
-
-    console.log(props)
-    const {title, description, image, price, _id, onAddToCart} = props;
-
+const ProductCard = (props) => {
+  console.log(props);
+  const { productName, imageUrl, price, _id, onAddToCart } = props;
 
   return (
-    <div className='border p-5 m-2 flex'>
-        <div>
-            <img src={`/assets/${image}`} alt={title} className='product-image p-5' />
+    <div className="border p-5 m-2 flex">
+      <div>
+        <img
+          src={`${imageUrl}`}
+          alt={productName}
+          className="product-image p-5"
+        />
+      </div>
+      <div className="product-details">
+        <Link to={`/product/${_id}`}>{productName}</Link>
+        <div className="product-price">
+          <p className="text-lg mr-5">£{price}</p>
+          <button className="btn btn-primary" onClick={onAddToCart}>
+            Add to Cart
+          </button>
         </div>
-        <div className='product-details'>
-            <Link to={`/product/${_id}`}>{title}</Link>
-            <div className='product-description'>
-                <p className='text-sm'>{description}</p>
-            </div>
-            <div className='product-price'>
-                <p className='text-lg mr-5'>£{price}</p>
-                <button className='btn btn-primary' onClick={onAddToCart}>Add to Cart</button>
-            </div>
-        </div>
+      </div>
     </div>
- )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
